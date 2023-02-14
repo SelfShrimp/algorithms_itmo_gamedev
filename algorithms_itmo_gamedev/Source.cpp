6f72b89a4c77890eaa46822f548c961cb505bc6b
Daniil Selyutin <daniil.selyutin@gmail.com>
2023-02-14 16:58:10 +0300

    базовый вывод информации

---
+++ algorithms_itmo_gamedev/Source.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>;
+#include <fstream>;
+
+using namespace std;
+
+unsigned short age = 1, population = 100, wheat = 2800, acres = 1000;
+
+void baseReport() {
+	cout << "Год: " << age << endl;
+	cout << "Кол-во населения: " << population << endl;
+	cout << "Кол-во пшеницы: " << wheat << endl;
+	cout << "Кол-во акров для посевов: " << acres << endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	baseReport();
+	while(true){}
+	return 0;
+}
