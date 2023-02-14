51f76d79692eec1e88af3eacef3a5a5013616222
Daniil Selyutin <daniil.selyutin@gmail.com>
2023-02-14 20:18:16 +0300

    лень писать нормальные коммиты, что то работает, кроме нормального вывода результатов и без сохранения

--- algorithms_itmo_gamedev/Source.cpp
+++ algorithms_itmo_gamedev/Source.cpp
@@ -4,7 +4,9 @@
 
 using namespace std;
 
-unsigned short age = 1, population = 100, wheat = 2800, acres = 1000, wheatForPeople = 0, wheatForSow = 0;
+unsigned short age = 1, population = 100, wheat = 2800, acres = 1000, wheatForPeople = 0, wheatForSow = 0, wheatPick = 0, wheatPerAcre = 1;
+short death = 0;
+float aveDeath = 0;
 bool isCorrect = false;
 string input = "";
 
@@ -17,6 +19,9 @@ void baseReport() {
 
 void toDo() {
 	short acreCost = rand() % 10 + 17;
+	wheatForPeople = 0;
+	wheatForSow = 0;
+
 	while (!isCorrect)
 	{
 		try {
@@ -30,14 +35,14 @@ void toDo() {
 			if (input[0] == '-') {
 				if (abs(stoi(input)) > acres) {
 					cout << "У вас не хватает акров" << endl;
-					break;
+					continue;
 				}
 			}
 
 			short cost = acreCost * stoi(input);
 			if (cost > wheat) {
 				cout << "У вас не хватает пшеницы" << endl;
-				break;
+				continue;
 			}
 			wheat -= cost;
 
@@ -59,15 +64,15 @@ void toDo() {
 			cin >> input;
 			if (stoi(input) == 0) {
 				isCorrect = true;
-				break;
+				continue;
 			}
 
 			if (input[0] == '-') {
 				cout << "Вы не можете отобрать у горожан то, чего у них нет" << endl;
-				break;
+				continue;
 			} else if (stoi(input) > wheat) {
 				cout << "У вас не хватает пшеницы" << endl;
-				break;
+				continue;
 			}
 			wheatForPeople += stoi(input);
 			wheat -= stoi(input);
@@ -92,11 +97,11 @@ void toDo() {
 
 			if (input[0] == '-') {
 				cout << "в земле нет пшеницы" << endl;
-				break;
+				continue;
 			}
 			else if (stoi(input) > wheat) {
 				cout << "У вас не хватает пшеницы" << endl;
-				break;
+				continue;
 			}
 			wheatForSow += stoi(input);
 			wheat -= stoi(input);
@@ -108,6 +113,60 @@ void toDo() {
 	}
 	input = "";
 	isCorrect = false;
+	cout << endl;
+}
+
+void resultPerYear() {
+	death = 0;
+	wheatPerAcre = rand() % 6 + 1;
+	wheatPick = 0;
+	if (wheatForSow * 2 > acres) {
+		cout << "вы выделили пшеницы больше, чем можете посадить, часть пшеницы пропала" << endl;
+		wheatPick += wheatPerAcre * acres;
+	}
+	else {
+		wheatPick += wheatPerAcre * wheatForSow * 2;
+	}
+	cout << "Мы собрали " << wheatPick << " пшеницы"<<endl;
+	wheat += wheatPick;
+	short wheatTaxForRat = (float)(rand() % 8) / 100 * wheat;
+	cout << "Крысы съели "<< wheatTaxForRat<< " пшеницы" << endl;
+	death = population - wheatForPeople / 20;
+	if (death < 0) death = 0;
+	aveDeath += (float) death / population;
+	population -= death;
+	cout << "Погибло: " << death << endl;
+}
+
+void newPeople() {
+	short people = death / 2 + (5 - wheatPerAcre) * wheat / 600 + 1;
+	if (people < 0) { people = 0; }
+	else if (people > 50) { people = 50; }
+	cout << "Прибыло: " << people << endl;
+	population += people;
+}
+
+void doom() {
+	float plagueChance = (static_cast<float>(rand() % 100)) / (static_cast<float>(100));
+	if (plagueChance <= 0.15)
+	{
+		population *= 0.5;
+		cout << "Случилась чума, население сократилось вдвое" << endl;
+	}
+	else
+		cout << "Чума обошла нас стороной в этом году" << endl;
+}
+
+void gameFinsihed() {
+	cout << "Вы продержались 10 лет!"<<endl;
+	cout << "Средняя смертность: " << aveDeath << endl;
+	short acresPerPeople = acres / population;
+	cout << "Acres per sitizen: " << acresPerPeople << endl;
+	if (aveDeath > 0.33 && acresPerPeople < 7.0) cout << "Плохо";
+	else if (aveDeath > 0.1 && acresPerPeople < 9.0) cout << "Нормально";
+	else if (aveDeath > 0.03 && acresPerPeople < 10.0) cout << "Хорошо";
+	else cout << "Отлично";
+	cout << endl;
 }
 
 int main() {
@@ -118,7 +177,17 @@ int main() {
 
 		baseReport();
 		toDo();
+		resultPerYear();
+		if ((float)death / population >= 0.45f || population <= 0) {
+			cout << "Из-за вашей некомпетентности в управлении, народ устроил бунт, и изгнал вас их города.Теперь вы вынуждены влачить жалкое существование в изгнании" << endl;
+			while(true){}
+			return 0;
+		}
+		newPeople();
+		doom();
 		age++;
 	}
+	gameFinsihed();
+	while(true){}
 	return 0;
 }
